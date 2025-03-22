6518470bd1dd7d40393f5b671e5a5f69d8c3c67b
AbhijeetNandvikar <abhijeetvn1234@gmail.com>
2025-03-22 20:02:34 +0530

    updates user model

--- market/models.py
+++ market/models.py
@@ -1,5 +1,15 @@
 from market import db
 
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username  = db.Column(db.String(length=30),nullable=False,unique=True)
+    password_hash = db.Column(db.String(length=60),nullable=False)
+    email = db.Column(db.String(length=50),nullable=False,unique=True)
+    budget = db.Column(db.Integer,nullable=False,default=1000)
+    items = db.relationship("Item", backref="owned_user",lazy=True)
+
+    def __repr__(self):
+        return f'item:{self.username}'
 
 
 class Item(db.Model):
@@ -8,7 +18,8 @@ class Item(db.Model):
     barcode = db.Column(db.Integer,nullable=False,unique=True)
     price = db.Column(db.Integer,nullable=True,unique=True)
     description = db.Column(db.String(length=1024),nullable=False,unique=True)
+    owner = db.Column(db.Integer,db.ForeignKey("user.id"))
 
-    # def __repr__(self):
-    #     return f'item:{self.name}'
+    def __repr__(self):
+        return f'item:{self.name}'
 

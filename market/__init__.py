1980c42aabb1928abafccf79b2eb91a1b16bdb6a
AbhijeetNandvikar <abhijeetvn1234@gmail.com>
2025-03-23 19:51:10 +0530

    adds user auth

--- market/__init__.py
+++ market/__init__.py
@@ -2,11 +2,23 @@ from flask import Flask, render_template
 
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_bcrypt import Bcrypt
+
+from flask_login import LoginManager
+
 app = Flask(__name__) # __name__ is a special variable in Python that is the name of the module.
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
+app.config['SECRET_KEY'] = 'f412a409ca8037768be8d586'
 
 db = SQLAlchemy(app)
 app.app_context().push()
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,10 @@
-from market import app
-from flask import render_template
-from market.models import Item
-
+import bcrypt
+from flask_login import login_user
+from market import app,db
+from flask import render_template,redirect,url_for,flash
+from market.models import Item,User
+from market.forms import LoginForm, RegisterUserForm
+from flask_login import login_user,logout_user,login_required
 
 @app.route('/') # This is a decorator that creates a mapping between the URL and the function.
 @app.route('/home')
@@ -14,3 +17,43 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+@app.route('/register',methods=['GET','POST'])
+def register_page():
+    form = RegisterUserForm()
+    if form.validate_on_submit():
+        # check if user exists
+        user_to_create = User(username=form.username.data,email_address=form.email_address.data,password=form.password1.data)
+        # add user to db
+        db.session.add(user_to_create)
+        # commit to db
+        db.session.commit()
+        # flash message
+        flash(f'Account created successfully! You are now able to login',category='success')
+        # redirect to login page
+        return redirect(url_for('login_page'))
+    
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    
+    return render_template('register.html',form=form)
+
+@app.route('/login',methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # check if user exists
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        # check if password is correct using bcrypt to compare the password_hash and the attempted_password
+        if attempted_user and attempted_user.check_password_correction(form.password.data):
+            # if the user exists and the password is correct, log the user in
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}',category='success')
+            # redirect to the market page
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password are not match! Please try again',category='danger')
+
+    return render_template('login.html',form=form)
